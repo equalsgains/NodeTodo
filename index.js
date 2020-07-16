b9f160b077d44d9b6e15fc9fa70917acb8ad2a09
Luis Quintanilla <equalsgains@gmail.com>
2020-07-16 14:01:43 -0600

    created models

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Oirigin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+  });
 
 app.use((req, res, next) => {
     res.send("welcome to express");
